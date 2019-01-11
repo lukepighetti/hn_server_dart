b2ee70de20fa251367085fa7c2cf671b28844933
Luke Pighetti <lukepighetti@gmail.com>
2019-01-10 20:50:19 -0500

    opeNode deployment setup

--- aqueduct/bin/server.dart
+++ aqueduct/bin/server.dart
@@ -1,9 +1,12 @@
 import 'package:hn_aqueduct/hn_aqueduct.dart';
+import 'dart:io' show Platform;
 
 Future main() async {
+  final port = int.parse(Platform.environment['PORT'] ?? "8080");
+
   final app = Application<HnAqueductChannel>()
     ..options.configurationFilePath = "config.yaml"
-    ..options.port = 8080;
+    ..options.port = port;
 
   final count = Platform.numberOfProcessors ~/ 2;
   await app.start(numberOfInstances: count > 0 ? count : 1);
