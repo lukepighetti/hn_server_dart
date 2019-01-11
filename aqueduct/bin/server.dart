6f8809f9fb87eedf8267a2a1f143afc497e63378
Luke Pighetti <lukepighetti@gmail.com>
2019-01-11 09:11:23 -0500

    INSTANCES environment variable

--- aqueduct/bin/server.dart
+++ aqueduct/bin/server.dart
@@ -1,15 +1,21 @@
 import 'package:hn_aqueduct/hn_aqueduct.dart';
 import 'dart:io' show Platform;
+import 'dart:math' show max;
 
 Future main() async {
+  /// Get properties from environment variables
   final port = int.parse(Platform.environment['PORT'] ?? "8080");
 
+  final instances = int.parse(
+    Platform.environment['INSTANCES'] ?? "${Platform.numberOfProcessors ~/ 2}",
+  );
+
+  /// Initialize app
   final app = Application<HnAqueductChannel>()
     ..options.configurationFilePath = "config.yaml"
     ..options.port = port;
 
-  final count = Platform.numberOfProcessors ~/ 2;
-  await app.start(numberOfInstances: count > 0 ? count : 1);
+  await app.start(numberOfInstances: max(instances, 1));
 
   print("Application started on port ${app.options.port}.");
   print("Use Ctrl-C (SIGINT) to stop running the application.");
