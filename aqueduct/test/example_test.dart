1eb005d48c324e34f2b2e3d52c39144265d35d88
Luke Pighetti <lukepighetti@gmail.com>
2019-01-12 18:02:20 -0500

    basic test suite

--- aqueduct/test/example_test.dart
+++ aqueduct/test/example_test.dart
@@ -3,12 +3,28 @@ import 'harness/app.dart';
 Future main() async {
   final harness = Harness()..install();
 
+  test("GET /articles/top/1 a list of objects with ids", () async {
+    expectResponse(
+      await harness.agent.get("/articles/top/1"),
+      200,
+      body: everyElement(contains("id")),
+    );
+  });
+
+  test("GET /comments/1 returns an object with kids", () async {
+    expectResponse(
+      await harness.agent.get("/comments/1"),
+      200,
+      body: predicate<Map>((m) => List<Map>.from(m['kids']).isNotEmpty),
+    );
+  });
+
   test("GET /articles/top returns 200", () async {
     expectResponse(await harness.agent.get("/articles/top"), 200);
   });
 
-  test("GET /articles/top/1 returns 200", () async {
-    expectResponse(await harness.agent.get("/articles/top/1"), 200);
+  test("GET /articles/top/999 returns 200", () async {
+    expectResponse(await harness.agent.get("/articles/top/999"), 200);
   });
 
   test("GET /comments/top/1 returns 404", () async {
--- aqueduct/test/harness/app.dart
+++ aqueduct/test/harness/app.dart
@@ -1,5 +1,4 @@
 import 'package:hn_aqueduct/channel.dart';
-import 'package:hn_aqueduct/classes.dart';
 
 import 'package:aqueduct_test/aqueduct_test.dart';
 
@@ -9,39 +8,10 @@ export 'package:test/test.dart';
 export 'package:aqueduct/aqueduct.dart';
 
 /// A testing harness for hn_aqueduct.
-///
-/// A harness for testing an aqueduct application. Example test file:
-///
-///         void main() {
-///           Harness harness = Harness()..install();
-///
-///           test("GET /path returns 200", () async {
-///             final response = await harness.agent.get("/path");
-///             expectResponse(response, 200);
-///           });
-///         }
-///
 class Harness extends TestHarness<HnAqueductChannel> {
   @override
-  Future onSetUp() async {
-    channel.api = HackerNewsTest();
-  }
+  Future onSetUp() async {}
 
   @override
   Future onTearDown() async {}
-}
-
-class HackerNewsTest implements HackerNewsInterface {
-  //
-  @override
-  Future<List<Map>> articles(ArticleView view, int page) {
-    // TODO: implement articles
-    return null;
-  }
-
-  @override
-  Future<Map> comments(int id) {
-    // TODO: implement comments
-    return null;
-  }
 }
