deefdadff263dfa0e1e2341395d64f3d34e6dd99
Luke Pighetti <lukepighetti@gmail.com>
2019-01-12 18:17:03 -0500

    cache now uses a single hashmap

--- aqueduct/lib/services/cache.dart
+++ aqueduct/lib/services/cache.dart
@@ -12,60 +12,58 @@ class Cache<K, V> {
   final Duration _timeout;
 
   /// Contains the cached values for each key
-  final _store = HashMap<K, V>();
-
-  /// Contains the timeout dates for each key
-  final _expirations = HashMap<K, DateTime>();
+  final _store = HashMap<K, _ExpiringValue<V>>();
 
   Cache([this._timeout = const Duration(minutes: 10)]) {
-    /// Purge stale keys to reduce memory usage
-    final purgeTime = _timeout;
-
     /// Setup a periodic purging of expired items
     Timer.periodic(
-      purgeTime,
+      _timeout,
       (_) => _purge(),
     );
   }
 
   /// Add an item to the store and set an expiration
   void _add(K key, V value) {
-    _store[key] = value;
-    _expirations[key] = DateTime.now().add(_timeout);
+    _store[key] = _ExpiringValue(value, _timeout);
   }
 
   /// Remove an item from the store and expire it
   void expire(K key) {
     _store.remove(key);
-    _expirations.remove(key);
   }
 
   /// Read an item from the store directly. Not recommended.
-  V read(K key) => _store[key];
+  V read(K key) => _store[key]?.value;
 
   /// The recommended method of reading. Provide a key and a fetching method.
   /// If the key has expired, it will automatically perform the fetching operation.
   ///
   /// Note: we cannot explicitly return `null` values
   Future<V> fetch(K key, FutureOr<V> futureValue) async {
-    final value = _store[key];
-    final expiration = _expirations[key];
-    final now = DateTime.now();
+    final item = _store[key];
 
-    if (value == null || expiration == null || now.isAfter(expiration)) {
+    if (item?.value == null || item.isExpired) {
       final newValue = await futureValue;
       _add(key, newValue);
       return newValue;
     }
 
-    return value;
+    return item?.value;
   }
 
   /// Search _expirations for expired keys and remove them from the cache.
   void _purge() {
-    final now = DateTime.now();
-
-    _store.removeWhere((key, value) => now.isAfter(_expirations[key]));
-    _expirations.removeWhere((key, expiration) => now.isAfter(expiration));
+    _store.removeWhere((key, value) => value.isExpired);
   }
+}
+
+/// A helper class for an expiring value
+class _ExpiringValue<T> {
+  final T value;
+  final DateTime expiration;
+
+  bool get isExpired => expiration.isBefore(DateTime.now());
+
+  _ExpiringValue(this.value, Duration timeout)
+      : expiration = DateTime.now().add(timeout);
 }
